13dccc25a6877f3f0522db3a039cb257e951358d
xiongchenyu <xiongchenyu6@gmail.com>
2024-07-16 17:06:55 +0800

    update flack

--- flake.nix
+++ flake.nix
@@ -1,60 +1,27 @@
 # SPDX-FileCopyrightText: 2021 Serokell <https://serokell.io/>
 #
 # SPDX-License-Identifier: CC0-1.0
-
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
-    crate2nix = {
-      url = "github:kolloch/crate2nix";
-      flake = false;
-    };
-    flake-utils.url = "github:numtide/flake-utils";
-    gitignore = {
-      url = "github:hercules-ci/gitignore.nix";
-      # Use the same nixpkgs
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { self, nixpkgs, crate2nix, flake-utils, gitignore }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        inherit (gitignore.lib) gitignoreSource;
-        # DON'T FORGET TO PUT YOUR PACKAGE NAME HERE, REMOVING `throw`
-        crateName = "learn_rust";
-
-        inherit (import "${crate2nix}/tools.nix" { inherit pkgs; })
-          generatedCargoNix;
-
-        project = import (generatedCargoNix {
-          name = crateName;
-          src = gitignoreSource ./.;
-        }) {
-          inherit pkgs;
-          defaultCrateOverrides = pkgs.defaultCrateOverrides // {
-            # Crate dependency overrides go here
-          };
-        };
-
-      in {
-        packages.${crateName} = project.rootCrate.build;
-
-        defaultPackage = self.packages.${system}.${crateName};
-
-        devShell = pkgs.mkShell {
-          XX = 1;
-          #inputsFrom = builtins.attrValues self.packages.${system};
-          buildInputs = [
-            pkgs.rustc
-            pkgs.cargo
-            pkgs.rust-analyzer
-            pkgs.clippy
-            pkgs.openssl
+  outputs = { nixpkgs, flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, lib, ... }: {
+        devShells.default =
+          with pkgs; mkShell{
+            buildInputs = [
+            rustc
+            cargo
+            rust-analyzer
+            clippy
+            openssl
           ];
-          nativeBuildInputs = [ pkgs.pkg-config ];
-
-        };
-      });
+            nativeBuildInputs = [ pkg-config ];
+         };
+      };
+    };
 }
